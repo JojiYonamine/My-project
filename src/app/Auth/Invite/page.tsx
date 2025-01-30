6e9c840d8a50f01266b2ed7b91b0112c21d5d6f3
jojiyonamine <yonaminejoji@gmail.com>
2025-01-30 15:01:26 +0900

    fix:UIのズレを修正、プロフィール設定できない問題解決

--- src/app/Auth/Invite/page.tsx
+++ src/app/Auth/Invite/page.tsx
@@ -2,26 +2,38 @@
 
 import { BasicButton } from "@/components";
 import useSignStore from "@/Context/signStore";
-import { getUserNameFromFirestore} from "@/utils/others/firestoreRefs";
+import { profileType } from "@/types/formtypes";
+import { getUserNameFromFirestore } from "@/utils/others/firestoreRefs";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
 
 const InvitePage = () => {
   const searhParams = useSearchParams();
   const root = useRouter();
-  const { setInviterId } = useSignStore()
+  const { setInviterId } = useSignStore();
   const inviterId = searhParams.get("inviterId");
   const [inviterName, setInviterName] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const setProgress = useSignStore((state) => state.setProgress);
+  const setProf = useSignStore((state) => state.setProfile);
 
-
+  const buttonFunc = () => {
+    const newProf: profileType = {
+      name: "",
+      icon: "",
+      birthday: new Date(2003, 0o4, 0o2),
+    };
+    setProf(newProf);
+    setProgress("name");
+    root.push(`/Auth/Login?inviterId=${inviterId}`);
+  };
 
   useEffect(() => {
     const fetchInviterName = async () => {
-      if (!inviterId) return
-      setInviterId(inviterId)
+      if (!inviterId) return;
+      setInviterId(inviterId);
       try {
-         {
+        {
           const name = await getUserNameFromFirestore(inviterId);
           setInviterName(name);
         }
@@ -38,15 +50,11 @@ const InvitePage = () => {
   return (
     <div className="h-screen flex justify-center text-center items-center">
       <div className="bg-white rounded p-6 w-11/12 sm:mt-0  max-w-lg border border-gray-300">
+        <h1 className="text-xl w-full mb-8 font-semibold break-words">ようこそ！</h1>
         <h1 className="text-xl w-full mb-8 font-semibold break-words">
-          ようこそ！
+          {inviterName ? `${inviterName}さんからの招待です！` : "招待者情報を取得中。。。"}
         </h1>
-        <h1 className="text-xl w-full mb-8 font-semibold break-words">
-          {inviterName
-            ? `${inviterName}さんからの招待です！`
-            : "招待者情報を取得中。。。"}
-        </h1>
-        <BasicButton onClick={()=>root.push(`/Auth/Login?inviterId=${inviterId}`)}>登録へ進む</BasicButton>
+        <BasicButton text="登録へ進む" onClick={() => buttonFunc()} />
       </div>
     </div>
   );
--- src/app/utils/Auth/loginHnandler.ts
+++ src/app/utils/Auth/loginHnandler.ts
@@ -1,6 +1,7 @@
 // ログイン・サインインを行う関数を返すカスタムフック
 import { auth } from "@/config/firebaseConfig";
 import useSignStore from "@/Context/signStore";
+import { formObjectType } from "@/types/formtypes";
 import { sendVerification } from "@/types/sendEmailVerification";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
@@ -10,7 +11,14 @@ export const useLogin = () => {
   const formError = useSignStore((state) => state.formError);
   const setLoading = useSignStore((state) => state.setLoading);
   const inviterId = useSignStore((state)=>state.inviterId)
+  const setFormObj = useSignStore((state)=>state.setFormObject)
   const root = useRouter()
+
+  const newForm: formObjectType = {
+    email: "",
+    password: "",
+    confirmPassword: null,
+  };
 
   const loginHandler = async (action: "login" | "signup",e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -38,6 +46,7 @@ export const useLogin = () => {
       console.log(err, "login handlerでエラー");
     }
     setLoading(false);
+    setFormObj(newForm)
   };
   return loginHandler
 };
